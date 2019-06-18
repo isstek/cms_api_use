8bef5e3df027e4e9e0558c7be36eaa0df67e3242
Igal Milman <igalmilman@gmail.com>
2019-06-18 13:53:34 -0400

    feat: FIrst release

---
+++ CMSServerConnect/CMSConnector.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace CMSServerConnect
+{
+    public class ClientClass
+    {
+        public string name;
+        public string unid;
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+
+    public class DomainClass
+    {
+        public string domain;
+        public string domain_long;
+        public string admin;
+        public string dns;
+
+        public override string ToString()
+        {
+            return domain;
+        }
+
+        public IPAddress IP
+        {
+            get
+            {
+                IPAddress result = null;
+                IPAddress.TryParse(dns, out result);
+                return result;
+            }
+        }
+    }
+
+    public class ClientsRequestAnswer
+    {
+        public string built;
+        public string csrf;
+        public List<ClientClass> client_list;
+    }
+
+    public class DomainRequestAnswer
+    {
+        public string built;
+        public string csrf;
+        public DomainClass domain;
+    }
+
+    public class CMSConnector
+    {
+        private const string CMS_SECRET_KEY_PROP_NAME = "secret_key";
+        private const string CSRF_CMS_SEND_PROP_NAME = "csrfmiddlewaretoken";
+
+        private const string CSRF_CMS_RECEIVE_PROP_NAME = "csrf";
+        private const string COMPUTER_ID_CMS_RECEIVE_PROP_NAME = "computerid";
+
+        string _server_name = "";
+        int _server_port = 443;
+        string _api_key = "";
+        string _csrf_token = "";
+        bool _use_https = true;
+        UriBuilder uri_base = null;
+        Uri baseUri = null;
+        CookieContainer _cookieContainer = null;
+
+        public CMSConnector(string api_key, string servername, int serverport = 443, bool https = true)
+        {
+            _api_key = api_key;
+            _server_name = servername;
+            _server_port = serverport;
+            _use_https = https;
+            _cookieContainer = new CookieContainer();
+            uri_base = new UriBuilder(_server_name);
+            baseUri = uri_base.Uri;
+            //uri_base.Port = _server_port;
+            //uri_base.Scheme = _use_https ? "https" : "http";
+        }
+
+        private bool GetCSRFToken(Dictionary<string, string> responseValues)
+        {
+            if (responseValues.ContainsKey(CSRF_CMS_RECEIVE_PROP_NAME))
+            {
+                _csrf_token = responseValues[CSRF_CMS_RECEIVE_PROP_NAME];
+                return true;
+            }
+            return false;
+        }
+
+        private bool InitConnection()
+        {
+            uri_base.Path = "api/check/";
+            uri_base.Query = CMS_SECRET_KEY_PROP_NAME + "=" + _api_key;
+            Uri uri = uri_base.Uri;
+            uri_base.Path = "";
+            uri_base.Query = "";
+            HttpStatusCode response_code;
+            string response = SendGetRequest(uri, out response_code);
+            if (response_code != HttpStatusCode.OK)
+            {
+                Console.WriteLine(response);
+                return false;
+            }
+            if (response is null)
+            {
+                return false;
+            }
+            Dictionary<string, string> responseValues = JsonConvert.DeserializeObject<Dictionary<string, string>>(response);
+
+            return GetCSRFToken(responseValues);
+        }
+
+        public int SendComputerInfo(string clientuuid, Dictionary<string, string> info)
+        {
+            if (!InitConnection())
+                return -1;
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("api/client/").Append(clientuuid).Append("/computer");
+            uri_base.Path = stringBuilder.ToString();
+            uri_base.Query = "";
+            Uri uri = uri_base.Uri;
+            uri_base.Path = "";
+            HttpStatusCode response_code;
+            Dictionary<string, string> data = new Dictionary<string, string>(info);
+            data.Add(CSRF_CMS_SEND_PROP_NAME, _cookieContainer.GetCookies(baseUri)["csrftoken"].Value);
+            data.Add(CMS_SECRET_KEY_PROP_NAME, _api_key);
+            string response = SendPOSTJsonRequest(uri, data, out response_code, true);
+            if (response_code != HttpStatusCode.OK)
+            {
+                Console.WriteLine(response);
+                return -1;
+            }
+            if (response is null)
+            {
+                return -1;
+            }
+            Dictionary<string, string> responseValues = JsonConvert.DeserializeObject<Dictionary<string, string>>(response);
+            int result = -1;
+            if (GetCSRFToken(responseValues) && responseValues.ContainsKey(COMPUTER_ID_CMS_RECEIVE_PROP_NAME) && int.TryParse(responseValues[COMPUTER_ID_CMS_RECEIVE_PROP_NAME], out result))
+                return result;
+
+            return -1;
+        }
+
+        public List<ClientClass> GetAllClients()
+        {
+            uri_base.Path = "api/clients";
+            uri_base.Query = CMS_SECRET_KEY_PROP_NAME + "=" + _api_key;
+            Uri uri = uri_base.Uri;
+            uri_base.Path = "";
+            uri_base.Query = "";
+            HttpStatusCode response_code;
+            string response = SendGetRequest(uri, out response_code);
+            if (response_code != HttpStatusCode.OK)
+            {
+                Console.WriteLine(response);
+                return null;
+            }
+            if (response is null)
+            {
+                return null;
+            }
+            try
+            {
+                ClientsRequestAnswer responseValues = JsonConvert.DeserializeObject<ClientsRequestAnswer>(response);
+                return responseValues.client_list;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR! {0}", e);
+                return null;
+            }
+        }
+
+        public List<ClientClass> GetDomainClients()
+        {
+            uri_base.Path = "api/domains";
+            uri_base.Query = CMS_SECRET_KEY_PROP_NAME + "=" + _api_key;
+            Uri uri = uri_base.Uri;
+            uri_base.Path = "";
+            uri_base.Query = "";
+            HttpStatusCode response_code;
+            string response = SendGetRequest(uri, out response_code);
+            if (response_code != HttpStatusCode.OK)
+            {
+                Console.WriteLine(response);
+                return null;
+            }
+            if (response is null)
+            {
+                return null;
+            }
+            try
+            {
+                ClientsRequestAnswer responseValues = JsonConvert.DeserializeObject<ClientsRequestAnswer>(response);
+                return responseValues.client_list;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR! {0}", e);
+                return null;
+            }
+        }
+
+        public DomainClass GetDomainInfo(ClientClass client)
+        {
+            uri_base.Path = "api/domain/" + client.unid;
+            uri_base.Query = CMS_SECRET_KEY_PROP_NAME + "=" + _api_key;
+            Uri uri = uri_base.Uri;
+            uri_base.Path = "";
+            uri_base.Query = "";
+            HttpStatusCode response_code;
+            string response = SendGetRequest(uri, out response_code);
+            if (response_code != HttpStatusCode.OK)
+            {
+                Console.WriteLine(response);
+                return null;
+            }
+            if (response is null)
+            {
+                return null;
+            }
+            try
+            {
+                DomainRequestAnswer responseValues = JsonConvert.DeserializeObject<DomainRequestAnswer>(response);
+                return responseValues.domain;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR! {0}", e);
+                return null;
+            }
+        }
+
+        private bool checkResponseCode(HttpStatusCode code, out string text)
+        {
+            switch (code)
+            {
+                case HttpStatusCode.OK:
+                    text = "OK";
+                    return true;
+                case HttpStatusCode.RequestTimeout:
+                case HttpStatusCode.ServiceUnavailable:
+                case HttpStatusCode.InternalServerError:
+                    text = new StringBuilder("Something wrong with the server. HTTP Code: ").Append((int)code).ToString();
+                    return false;
+                case HttpStatusCode.PaymentRequired:
+                    text = "Bad request: API Key needed";
+                    return false;
+                case HttpStatusCode.Unauthorized:
+                    text = "Bad request: API Key is not valid";
+                    return false;
+                case HttpStatusCode.Forbidden:
+                    text = "Bad request: CSRF token is not provided or invalid";
+                    return false;
+                case HttpStatusCode.BadRequest:
+                    text = "Bad request: Some information in the body of the request is not right";
+                    return false;
+                case HttpStatusCode.Unused:
+                    text = "Something is wrong with the server. Server is not reponding or is turned off.";
+                    return false;
+                default:
+                    text = new StringBuilder("Unknown response code. HTTP Code: ").Append((int)code).ToString();
+                    return false;
+            }
+        }
+
+        private string SendGetRequest(Uri uri, out HttpStatusCode responseStatusCode, bool save_cookies = true)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Method = "GET";
+            request.ContentType = null;
+            request.CookieContainer = _cookieContainer;
+            HttpWebResponse response = null;
+            HttpStatusCode statusCode;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+                statusCode = response.StatusCode;
+            }
+            catch (WebException we)
+            {
+                if (we.Response is null)
+                    statusCode = HttpStatusCode.Unused;
+                else
+                    statusCode = ((HttpWebResponse)we.Response).StatusCode;
+            }
+            responseStatusCode = statusCode;
+            string response_code_text;
+            if (!checkResponseCode(statusCode, out response_code_text))
+            {
+                return response_code_text;
+            }
+            if (save_cookies)
+            {
+                for (int i = 0; i < response.Cookies.Count; i++)
+                {
+                    response.Cookies[i].Path = String.Empty;
+                }
+                uri_base.Path = "";
+                _cookieContainer = new CookieContainer();
+                _cookieContainer.Add(baseUri, response.Cookies);
+            }
+            string responseText;
+            using (var reader = new System.IO.StreamReader(response.GetResponseStream()))
+            {
+                responseText = reader.ReadToEnd();
+            }
+            return responseText;
+        }
+
+        private string SendPOSTJsonRequest(Uri uri, Dictionary<string, string> data, out HttpStatusCode responseStatusCode, bool save_cookies = true)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded; charset=utf-8";
+            request.CookieContainer = _cookieContainer;
+            request.Referer = uri.ToString();
+            NameValueCollection outgoingQueryString = HttpUtility.ParseQueryString(String.Empty);
+            if (data != null)
+            {
+                foreach (KeyValuePair<string, string> entry in data)
+                {
+                    outgoingQueryString.Add(entry.Key, entry.Value);
+                }
+            }
+            string strData = outgoingQueryString.ToString();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] byte1 = encoding.GetBytes(strData);
+            request.ContentLength = byte1.Length;
+            using (StreamWriter streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(strData);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            HttpWebResponse response = null;
+            HttpStatusCode statusCode;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+                statusCode = response.StatusCode;
+            }
+            catch (WebException we)
+            {
+                if (we.Response is null)
+                    statusCode = HttpStatusCode.Unused;
+                else
+                    statusCode = ((HttpWebResponse)we.Response).StatusCode;
+            }
+            responseStatusCode = statusCode;
+            string response_code_text;
+            if (!checkResponseCode(statusCode, out response_code_text))
+            {
+                return response_code_text;
+            }
+            if (save_cookies)
+            {
+                for (int i = 0; i < response.Cookies.Count; i++)
+                {
+                    response.Cookies[i].Path = String.Empty;
+                }
+                uri_base.Path = "";
+                _cookieContainer = new CookieContainer();
+                _cookieContainer.Add(baseUri, response.Cookies);
+            }
+            string responseText;
+            using (var reader = new System.IO.StreamReader(response.GetResponseStream()))
+            {
+                responseText = reader.ReadToEnd();
+            }
+            return responseText;
+        }
+
+    }
+}
---
+++ JoinDomain/AddToDomain.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Net;
+using System.Management;
+using System.Runtime.InteropServices;
+
+namespace JoinDomain
+{
+    public class AddToDomain
+    {
+        [DllImport("dnsapi.dll", EntryPoint = "DnsFlushResolverCache")]
+        private static extern UInt32 DnsFlushResolverCache();
+
+        private IPAddress _dns;
+        private string _name;
+        private string _long_name;
+        private string _admin_user;
+        private string _admin_password;
+
+        private int _connect_to_domain_result;
+
+        public AddToDomain(IPAddress dnsip, string domain_name, string domain_name_long, string admin_user)
+        {
+            _dns = dnsip;
+            _name = domain_name;
+            _long_name = domain_name_long;
+            _admin_password = "";
+            _admin_user = admin_user;
+        }
+
+        public string Password
+        {
+            set
+            {
+                _admin_password = value;
+            }
+        }
+
+        public string fullAdminUsername
+        {
+            get
+            {
+                return _name + "\\" + _admin_user;
+            }
+        }
+
+        private void SetDnsConfig(bool resetall = true)
+        {
+            ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection moc = mc.GetInstances();
+            foreach (ManagementObject mo in moc)
+            {
+                if ((bool)mo["IPEnabled"])
+                {
+                    if (resetall || mo["DefaultIPGateway"] != null)
+                    {
+                        ManagementBaseObject DnsEntry = mo.GetMethodParameters("SetDNSServerSearchOrder");
+                        DnsEntry["DNSServerSearchOrder"] = new string[] { _dns.ToString(), "8.8.8.8" };
+                        ManagementBaseObject DnsMbo = mo.InvokeMethod("SetDNSServerSearchOrder", DnsEntry, null);
+                        int returnCode = int.Parse(DnsMbo["returnvalue"].ToString());
+                    }
+                }
+            }
+            for (int i = 0; i < 4; ++i)
+                DnsFlushResolverCache().ToString();
+        }
+
+        public string ErrorText
+        {
+            get
+            {
+                switch (_connect_to_domain_result)
+                {
+                    case 0:
+                        return "Success";
+                    case 5:
+                        return "Access is denied. Please run software as administrator.";
+                    case 87:
+                        return "One of the parameters is incorrect.";
+                    case 110:
+                        return "Could not access the specified object.";
+                    case 1323:
+                        return "Unable to update the password.";
+                    case 1326:
+                        return "Logon failure: unknown username or bad password.";
+                    case 1355:
+                        return "Specified domain does not exist or could not be contacted.";
+                    case 2224:
+                        return "The account already exists.";
+                    case 2691:
+                        return "The machine is already joined the domain.";
+                    case 2692:
+                        return "The machine is not currently joined the domain.";
+                    default:
+                        return "Unknow error";
+                }
+            }
+        }
+
+        private bool LeaveDomain()
+        {
+            // Invoke WMI to join the domain
+            using (ManagementObject wmiObject = new ManagementObject(new ManagementPath("Win32_ComputerSystem.Name='" + System.Environment.MachineName + "'")))
+            {
+                try
+                {
+                    // Obtain in-parameters for the method
+                    ManagementBaseObject inParams = wmiObject.GetMethodParameters("UnJoinDomainOrWorkgroup");
+
+                    inParams["FJoinOptions"] = 0;
+
+                    // Execute the method and obtain the return values.
+                    ManagementBaseObject outParams = wmiObject.InvokeMethod("JoinDomainOrWorkgroup", inParams, null);
+
+                    //_connect_to_domain_result = (int)outParams["ReturnValue"];
+
+                    return (int)outParams["ReturnValue"] == 0;
+                }
+                catch (ManagementException e)
+                {
+                    //_connect_to_domain_result = 4;
+                    return false;
+                }
+            }
+        }
+
+        private bool EnterDomain()
+        {
+            // Invoke WMI to join the domain
+            using (ManagementObject wmiObject = new ManagementObject(new ManagementPath("Win32_ComputerSystem.Name='" + System.Environment.MachineName + "'")))
+            {
+                try
+                {
+                    // Obtain in-parameters for the method
+                    ManagementBaseObject inParams = wmiObject.GetMethodParameters("JoinDomainOrWorkgroup");
+
+                    inParams["Name"] = _long_name;
+                    inParams["Password"] = _admin_password;
+                    inParams["UserName"] = fullAdminUsername;
+                    inParams["FJoinOptions"] = 3; // Magic number: 3 = join to domain and create computer account
+
+                    // Execute the method and obtain the return values.
+                    ManagementBaseObject outParams = wmiObject.InvokeMethod("JoinDomainOrWorkgroup", inParams, null);
+
+                    _connect_to_domain_result = (int)outParams["ReturnValue"];
+
+                    return _connect_to_domain_result == 0;
+                }
+                catch (ManagementException e)
+                {
+                    _connect_to_domain_result = 4;
+                    return false;
+                }
+            }
+        }
+
+        public bool joinDomain()
+        {
+            SetDnsConfig();
+            if (System.Environment.UserDomainName != "")
+                LeaveDomain();
+            return EnterDomain();
+        }
+    }
+}
